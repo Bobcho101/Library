50350849c9753135a728cec2c8e93caadac50f2c
Bozhidar Tsvetkov <bgbobi878@gmail.com>
2025-11-04 20:57:59 +0200

    fixed paths

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+// import routes from './routes.js';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import handlebars from 'express-handlebars';
+// import { authMiddleware } from './middlewares/auth-middleware.js';
+const app = express();
+const PORT = 3000;
+
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true
+    }
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static('src/public'))
+
+
+// try{
+//     const localUri = mongoUri;
+//     await mongoose.connect(localUri);
+// } catch(err){
+//     console.log(err.message);
+// }
+
+
+app.get('/', (req, res) => {
+    res.send("hi")
+})
+// app.use(authMiddleware);
+// app.use(routes);
+
+app.listen(PORT, console.log(`http://localhost:${PORT}`));
