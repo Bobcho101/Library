f20542374a3cf3bd58f3d1cbb2e2397d0b49b03a
Bozhidar Tsvetkov <bgbobi878@gmail.com>
2025-12-15 17:45:19 +0200

    added create and getone functions

--- src/services/booksService.js
+++ src/services/booksService.js
@@ -3,3 +3,11 @@ import Book from "../models/BooksModel.js";
 export async function getBooks() {
     return await Book.find();
 }
+
+export async function createBook(formData) {
+    return await Book.create(formData);
+}
+
+export async function getOneBook(id) {
+    return await Book.findById(id)
+}
